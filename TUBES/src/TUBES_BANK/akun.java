107d26088fc90ae968eec4aafbc75e655e0e4e22
ssilllvii <155700774+ssilllvii@users.noreply.github.com>
2024-01-07 10:06:13 +0700

    Delete TUBES directory

--- TUBES/src/TUBES_BANK/akun.java
+++
@@ -1,9 +0,0 @@
-package TUBES_BANK;
-public interface akun {
-    String getAccountNumber();      // 
-    String getAccountHolderName();
-    double getBalance();
-    void displayAccountInfo();
-    void withdraw(double amount);
-    void deposit(double amount);
-} 
